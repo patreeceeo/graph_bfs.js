803e3705c038acf721ad26bc01d66cebb27cad5d
Patrick Canfield <pscale01@gmail.com>
2023-10-10 10:38:54 -0700

    update package name

--- dist/graph.d.ts
+++ dist/graph.d.ts
@@ -1,5 +1,5 @@
 
-declare module "graph_fns" {
+declare module "graph_fn" {
   export type Graph = Map<string, Set<string>>;
   export function searchBreadth(graph: Graph, start: string): Set<string>;
 }
